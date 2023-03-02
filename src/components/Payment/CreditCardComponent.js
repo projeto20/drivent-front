33a6581461afe3e3425000e73faf394685b9962c
EmanuelMarcolongo <emanuelmarcolongo@gmail.com>
2023-03-02 09:31:11 -0300

    feat: payment checkout in separated component

--- src/components/Payment/CreditCardComponent.js
+++ src/components/Payment/CreditCardComponent.js
@@ -58,6 +58,7 @@ export default class App extends React.Component {
 
     this.setState({ formData });
     this.form.reset();
+    console.log(formData);
   };
 
   render() {
---
+++ src/components/Payment/PaymentWithCard.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { Title } from '../Auth';
+import { PaymentConfirmedContainer, Subtitle, TicketInfoContainer } from './PaymentStyledComponents';
+import checkbox from '../../assets/images/Vector.svg';
+import CreditCardComponent from './CreditCardComponent';
+import 'react-credit-cards/es/styles-compiled.css';
+
+export default function PaymentWithCard() {
+  const ticket = { isRemote: true, containsHotel: false, price: 600.0 };
+  const [paid, setPaid] = useState(false);
+
+  return (
+    <>
+      <Title>Ingresso e pagamento</Title>
+      <Subtitle>Ingresso Escolhido</Subtitle>
+      <TicketInfoContainer>
+        <p>{ticket.isRemote ? 'Remoto' : ticket.containsHotel ? 'Presencial + Com Hotel' : 'Presencial + Sem Hotel'}</p>
+        <h3>R$ {ticket.price}</h3>
+      </TicketInfoContainer>
+      <Subtitle>Pagamento</Subtitle>
+      <CreditCardComponent setPaid={setPaid}/>
+      {paid ? <PaymentConfirmed /> : ''}
+    </>
+  );
+}
+
+function PaymentConfirmed() {
+  return (
+    <PaymentConfirmedContainer>
+      <img src={checkbox} alt="checked checkbox" />
+      <div>
+        <p>Pagamento Confirmado</p>
+        <p>Prossiga para escolha de hospedagem e atividades</p>
+      </div>
+    </PaymentConfirmedContainer>
+  );
+}
